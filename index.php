ed50e8a80449bfef2bb9159f1c1b7afb614f1d3e
unknown <Administrator@Fazil-PC.VACKER.NET>
2016-04-26 12:51:46 +0400

    Reverted

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-/** this is test only.
+/**
  * Front to the WordPress application. This file doesn't do anything, but loads
  * wp-blog-header.php which does and tells WordPress to load the theme.
  *
